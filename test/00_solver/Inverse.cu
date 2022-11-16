fb0c04034116f64151610bf505672b15959d0fc4
cliffburdick <cburdick@nvidia.com>
2022-11-16 08:21:46 -0800

    Added batched inverse tests

--- test/00_solver/Inverse.cu
+++ test/00_solver/Inverse.cu
@@ -58,6 +58,68 @@ class InvSolverTestFloatTypes : public InvSolverTest<TensorType> {
 TYPED_TEST_SUITE(InvSolverTestFloatTypes,
                  MatXFloatNonHalfTypes);
 
+TYPED_TEST(InvSolverTestFloatTypes, Inv4x4)
+{
+  MATX_ENTER_HANDLER();
+
+  auto A = make_tensor<TypeParam>({4, 4});
+  auto Ainv = make_tensor<TypeParam>({4, 4});
+  auto Ainv_ref = make_tensor<TypeParam>({4, 4});  
+
+  this->pb->template InitAndRunTVGenerator<TypeParam>("00_solver", "inv", "run", {4, 1});
+  this->pb->NumpyToTensorView(A, "A");
+  this->pb->NumpyToTensorView(Ainv_ref, "A_inv");  
+
+  inv(Ainv, A, 0);
+  cudaStreamSynchronize(0);
+
+  for (index_t i = 0; i < A.Size(0); i++) {
+    for (index_t j = 0; j <= i; j++) {
+      if constexpr (is_complex_v<TypeParam>) {
+        ASSERT_NEAR(Ainv_ref(i, j).real(), Ainv(i, j).real(), 0.001);
+        ASSERT_NEAR(Ainv_ref(i, j).imag(), Ainv(i, j).imag(), 0.001);
+      }
+      else {
+        ASSERT_NEAR(Ainv_ref(i, j), Ainv(i, j), 0.001);
+      }
+    }
+  }
+
+  MATX_EXIT_HANDLER();
+}        
+
+TYPED_TEST(InvSolverTestFloatTypes, Inv4x4Batched)
+{
+  MATX_ENTER_HANDLER();
+
+  auto A = make_tensor<TypeParam>({100, 4, 4});
+  auto Ainv = make_tensor<TypeParam>({100, 4, 4});
+  auto Ainv_ref = make_tensor<TypeParam>({100, 4, 4});  
+
+  this->pb->template InitAndRunTVGenerator<TypeParam>("00_solver", "inv", "run", {4, 100});
+  this->pb->NumpyToTensorView(A, "A");
+  this->pb->NumpyToTensorView(Ainv_ref, "A_inv");  
+
+  inv(Ainv, A, 0);
+  cudaStreamSynchronize(0);
+
+  for (index_t b = 0; b < A.Size(0); b++) {
+    for (index_t i = 0; i < A.Size(1); i++) {
+      for (index_t j = 0; j <= i; j++) {
+        if constexpr (is_complex_v<TypeParam>) {
+          ASSERT_NEAR(Ainv_ref(b, i, j).real(), Ainv(b, i, j).real(), 0.001);
+          ASSERT_NEAR(Ainv_ref(b, i, j).imag(), Ainv(b, i, j).imag(), 0.001);
+        }
+        else {
+          ASSERT_NEAR(Ainv_ref(b, i, j), Ainv(b, i, j), 0.001);
+        }
+      }
+    }
+  }
+
+  MATX_EXIT_HANDLER();
+}    
+
 TYPED_TEST(InvSolverTestFloatTypes, Inv8x8)
 {
   MATX_ENTER_HANDLER();
@@ -66,7 +128,7 @@ TYPED_TEST(InvSolverTestFloatTypes, Inv8x8)
   auto Ainv = make_tensor<TypeParam>({8, 8});
   auto Ainv_ref = make_tensor<TypeParam>({8, 8});  
 
-  this->pb->template InitAndRunTVGenerator<TypeParam>("00_solver", "inv", "run", {8});
+  this->pb->template InitAndRunTVGenerator<TypeParam>("00_solver", "inv", "run", {8, 1});
   this->pb->NumpyToTensorView(A, "A");
   this->pb->NumpyToTensorView(Ainv_ref, "A_inv");  
 
@@ -88,6 +150,38 @@ TYPED_TEST(InvSolverTestFloatTypes, Inv8x8)
   MATX_EXIT_HANDLER();
 }
 
+TYPED_TEST(InvSolverTestFloatTypes, Inv8x8Batched)
+{
+  MATX_ENTER_HANDLER();
+
+  auto A = make_tensor<TypeParam>({100, 8, 8});
+  auto Ainv = make_tensor<TypeParam>({100, 8, 8});
+  auto Ainv_ref = make_tensor<TypeParam>({100, 8, 8});  
+
+  this->pb->template InitAndRunTVGenerator<TypeParam>("00_solver", "inv", "run", {8, 100});
+  this->pb->NumpyToTensorView(A, "A");
+  this->pb->NumpyToTensorView(Ainv_ref, "A_inv");  
+
+  inv(Ainv, A, 0);
+  cudaStreamSynchronize(0);
+
+  for (index_t b = 0; b < A.Size(0); b++) {
+    for (index_t i = 0; i < A.Size(1); i++) {
+      for (index_t j = 0; j <= i; j++) {
+        if constexpr (is_complex_v<TypeParam>) {
+          ASSERT_NEAR(Ainv_ref(b, i, j).real(), Ainv(b, i, j).real(), 0.001);
+          ASSERT_NEAR(Ainv_ref(b, i, j).imag(), Ainv(b, i, j).imag(), 0.001);
+        }
+        else {
+          ASSERT_NEAR(Ainv_ref(b, i, j), Ainv(b, i, j), 0.001);
+        }
+      }
+    }
+  }
+
+  MATX_EXIT_HANDLER();
+}    
+
 TYPED_TEST(InvSolverTestFloatTypes, Inv256x256)
 {
   MATX_ENTER_HANDLER();
@@ -97,7 +191,7 @@ TYPED_TEST(InvSolverTestFloatTypes, Inv256x256)
   auto Ainv = make_tensor<TypeParam>({256, 256});
   auto Ainv_ref = make_tensor<TypeParam>({256, 256});  
 
-  this->pb->template InitAndRunTVGenerator<TypeParam>("00_solver", "inv", "run", {256});
+  this->pb->template InitAndRunTVGenerator<TypeParam>("00_solver", "inv", "run", {256, 1});
   this->pb->NumpyToTensorView(A, "A");
   this->pb->NumpyToTensorView(Ainv_ref, "A_inv");  
 
