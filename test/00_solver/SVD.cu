6a3ecd385044cc154d4cd74bf7b1450ca449fd0e
cliffburdick <cburdick@nvidia.com>
2023-01-04 13:48:18 -0800

    Fixed batching on solver interface and added SVD batching unit test

--- test/00_solver/SVD.cu
+++ test/00_solver/SVD.cu
@@ -46,26 +46,11 @@ protected:
   {
     pb = std::make_unique<detail::MatXPybind>();
     pb->InitAndRunTVGenerator<T>("00_solver", "svd", "run", {m, n});
-    pb->NumpyToTensorView(Av, "A");
   }
 
   void TearDown() { pb.reset(); }
-  using scalar_type = typename inner_op_type_t<T>::type;
 
   std::unique_ptr<detail::MatXPybind> pb;
-  tensor_t<T, 2> Av{{m, n}};
-  tensor_t<T, 2> Atv{{n, m}};
-  tensor_t<scalar_type, 1> Sv{{std::min(m, n)}};
-  tensor_t<T, 2> Uv{{m, m}};
-  tensor_t<T, 2> Vv{{n, n}};
-
-  tensor_t<scalar_type, 2> Sav{{m, n}};
-  tensor_t<T, 2> SSolav{{m, n}};
-  tensor_t<T, 2> Uav{{m, m}};
-  tensor_t<T, 2> Vav{{n, n}};
-
-  // Used only for validation
-  tensor_t<T, 2> tmpV{{m, n}};
 };
 
 template <typename TensorType>
@@ -79,12 +64,29 @@ TYPED_TEST(SVDSolverTestNonHalfTypes, SVDBasic)
 {
   MATX_ENTER_HANDLER();
 
+  using scalar_type = typename inner_op_type_t<TypeParam>::type;
+  tensor_t<TypeParam, 2> Av{{m, n}};
+  tensor_t<TypeParam, 2> Atv{{n, m}};
+  tensor_t<scalar_type, 1> Sv{{std::min(m, n)}};
+  tensor_t<TypeParam, 2> Uv{{m, m}};
+  tensor_t<TypeParam, 2> Vv{{n, n}};
+
+  tensor_t<scalar_type, 2> Sav{{m, n}};
+  tensor_t<TypeParam, 2> SSolav{{m, n}};
+  tensor_t<TypeParam, 2> Uav{{m, m}};
+  tensor_t<TypeParam, 2> Vav{{n, n}};
+
+  this->pb->NumpyToTensorView(Av, "A");
+
+  // Used only for validation
+  tensor_t<TypeParam, 2> tmpV{{m, n}};
+
   // cuSolver only supports col-major solving today, so we need to transpose,
   // solve, then transpose again to compare to Python
-  transpose(this->Atv, this->Av, 0);
+  transpose(Atv, Av, 0);
 
-  auto Atv2 = this->Atv.View({m, n});
-  svd(this->Uv, this->Sv, this->Vv, Atv2);
+  auto Atv2 = Atv.View({m, n});
+  svd(Uv, Sv, Vv, Atv2);
 
   cudaStreamSynchronize(0);
 
@@ -92,40 +94,123 @@ TYPED_TEST(SVDSolverTestNonHalfTypes, SVDBasic)
   // compare against Python output. Instead, we just make sure that A = U*S*V'.
   // However, U and V are in column-major format, so we have to transpose them
   // back to verify the identity.
-  transpose(this->Uav, this->Uv, 0);
-  transpose(this->Vav, this->Vv, 0);
+  transpose(Uav, Uv, 0);
+  transpose(Vav, Vv, 0);
 
   // Zero out s
-  (this->Sav = zeros<typename inner_op_type_t<TypeParam>::type>({m, n})).run();
+  (Sav = zeros<typename inner_op_type_t<TypeParam>::type>({m, n})).run();
   cudaStreamSynchronize(0);
 
   // Construct S matrix since it's just a vector from cuSolver
   for (index_t i = 0; i < n; i++) {
-    this->Sav(i, i) = this->Sv(i);
+    Sav(i, i) = Sv(i);
   }
 
   cudaStreamSynchronize(0);
 
-  (this->SSolav = 0).run();
+  (SSolav = 0).run();
   if constexpr (is_complex_v<TypeParam>) {
-    (this->SSolav.RealView() = this->Sav).run();
+    (SSolav.RealView() = Sav).run();
   }
   else {
-    (this->SSolav = this->Sav).run();
+    (SSolav = Sav).run();
   }
 
-  matmul(this->tmpV, this->Uav, this->SSolav); // U * S
-  matmul(this->SSolav, this->tmpV, this->Vav); // (U * S) * V'
+  matmul(tmpV, Uav, SSolav); // U * S
+  matmul(SSolav, tmpV, Vav); // (U * S) * V'
   cudaStreamSynchronize(0);
 
-  for (index_t i = 0; i < this->Av.Size(0); i++) {
-    for (index_t j = 0; j < this->Av.Size(1); j++) {
+  for (index_t i = 0; i < Av.Size(0); i++) {
+    for (index_t j = 0; j < Av.Size(1); j++) {
       if constexpr (is_complex_v<TypeParam>) {
-        ASSERT_NEAR(this->Av(i, j).real(), this->SSolav(i, j).real(), 0.001) << i << " " << j;
-        ASSERT_NEAR(this->Av(i, j).imag(), this->SSolav(i, j).imag(), 0.001) << i << " " << j;
+        ASSERT_NEAR(Av(i, j).real(), SSolav(i, j).real(), 0.001) << i << " " << j;
+        ASSERT_NEAR(Av(i, j).imag(), SSolav(i, j).imag(), 0.001) << i << " " << j;
       }
       else {
-        ASSERT_NEAR(this->Av(i, j), this->SSolav(i, j), 0.001) << i << " " << j;
+        ASSERT_NEAR(Av(i, j), SSolav(i, j), 0.001) << i << " " << j;
+      }
+    }
+  }
+
+  MATX_EXIT_HANDLER();
+}
+
+TYPED_TEST(SVDSolverTestNonHalfTypes, SVDBasicBatched)
+{
+  MATX_ENTER_HANDLER();
+
+  constexpr index_t batches = 10;
+
+  using scalar_type = typename inner_op_type_t<TypeParam>::type;
+  auto Av1 = make_tensor<TypeParam>({m, n});
+  this->pb->NumpyToTensorView(Av1, "A");
+  auto Av = make_tensor<TypeParam>({batches, m, n});
+  auto Atv = make_tensor<TypeParam>({batches, n, m});
+  (Av = Av1).run();  
+
+  auto Sv = make_tensor<scalar_type>({batches, std::min(m, n)});
+  auto Uv = make_tensor<TypeParam>({batches, m, m});
+  auto Vv = make_tensor<TypeParam>({batches, n, n});
+
+  auto Sav = make_tensor<scalar_type>({batches, m, n});
+  auto SSolav = make_tensor<TypeParam>({batches, m, n});
+  auto Uav = make_tensor<TypeParam>({batches, m, m});
+  auto Vav = make_tensor<TypeParam>({batches, n, n});
+
+  // Used only for validation
+  auto tmpV = make_tensor<TypeParam>({batches, m, n});
+
+  // cuSolver only supports col-major solving today, so we need to transpose,
+  // solve, then transpose again to compare to Python
+  transpose(Atv, Av, 0);
+
+  auto Atv2 = Atv.View({batches, m, n});
+  svd(Uv, Sv, Vv, Atv2);
+
+  cudaStreamSynchronize(0);
+
+  // Since SVD produces a solution that's not necessarily unique, we cannot
+  // compare against Python output. Instead, we just make sure that A = U*S*V'.
+  // However, U and V are in column-major format, so we have to transpose them
+  // back to verify the identity.
+  transpose(Uav, Uv, 0);
+  transpose(Vav, Vv, 0);
+
+  // Zero out s
+  (Sav = zeros<typename inner_op_type_t<TypeParam>::type>({batches, m, n})).run();
+  cudaStreamSynchronize(0);
+
+  // Construct S matrix since it's just a vector from cuSolver
+  for (index_t b = 0; b < batches; b++) {
+    for (index_t i = 0; i < n; i++) {
+      Sav(b, i, i) = Sv(b, i);
+    }
+  }
+
+  cudaStreamSynchronize(0);
+
+  (SSolav = 0).run();
+  if constexpr (is_complex_v<TypeParam>) {
+    (SSolav.RealView() = Sav).run();
+  }
+  else {
+    (SSolav = Sav).run();
+  }
+
+  matmul(tmpV, Uav, SSolav); // U * S
+  matmul(SSolav, tmpV, Vav); // (U * S) * V'
+  cudaStreamSynchronize(0);
+
+  for (index_t b = 0; b < batches; b++) {
+    for (index_t i = 0; i < Av.Size(0); i++) {
+      for (index_t j = 0; j < Av.Size(1); j++) {
+        if constexpr (is_complex_v<TypeParam>) {
+          ASSERT_NEAR(Av(b, i, j).real(), SSolav(b, i, j).real(), 0.001) << i << " " << j;
+          ASSERT_NEAR(Av(b, i, j).imag(), SSolav(b, i, j).imag(), 0.001) << i << " " << j;
+        }
+        else {
+          ASSERT_NEAR(Av(b, i, j), SSolav(b, i, j), 0.001) << i << " " << j;
+        }
       }
     }
   }
