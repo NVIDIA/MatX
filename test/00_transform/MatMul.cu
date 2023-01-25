99c2bc58721d262d2c065dd64c4eea3998ba64c8
kshitij12345 <kshitijkalambarkar@gmail.com>
2023-01-25 19:37:17 +0000

    update test

--- test/00_transform/MatMul.cu
+++ test/00_transform/MatMul.cu
@@ -353,30 +353,29 @@ TYPED_TEST(MatMulTestFloatTypes, MediumRectBatched3DStridedBatch)
 
 TYPED_TEST(MatMulTestFloatNonComplexTypes, MixedTypes)
 {
+  // a -> complex, b -> real, c -> complex
+  MATX_ENTER_HANDLER();
+
+  constexpr index_t m = 4;
+  constexpr index_t k = 8;
+  constexpr index_t n = 16;
+
   using ComplexTypeParam = float_to_complex_t<TypeParam>;
-  // a -> real, b -> complex, c -> complex
-  {
-    MATX_ENTER_HANDLER();
 
-    constexpr index_t m = 4;
-    constexpr index_t k = 8;
-    constexpr index_t n = 16;
+  tensor_t<ComplexTypeParam, 2> a{{m, k}};
+  tensor_t<TypeParam, 2> b{{k, n}};
+  tensor_t<ComplexTypeParam, 2> c{{m, n}};
 
-    tensor_t<TypeParam, 2> a{{m, k}};
-    tensor_t<ComplexTypeParam, 2> b{{k, n}};
-    tensor_t<ComplexTypeParam, 2> c{{m, n}};
+  this->pb->template InitAndRunTVGenerator<TypeParam>(
+      "00_transforms", "matmul_operators", "run_mixed", {m, k, n});
 
-    this->pb->template InitAndRunTVGenerator<TypeParam>(
-        "00_transforms", "matmul_operators", "run", {m, k, n});
+  this->pb->NumpyToTensorView(a, "a");
+  this->pb->NumpyToTensorView(b, "b");
 
-    this->pb->NumpyToTensorView(a, "a");
-    this->pb->NumpyToTensorView(b, "b");
+  matmul(c, a, b);
+  MATX_TEST_ASSERT_COMPARE(this->pb, c, "c", this->thresh);
 
-    matmul(c, a, b);
-    MATX_TEST_ASSERT_COMPARE(this->pb, c, "c", this->thresh);
-
-    MATX_EXIT_HANDLER();
-  }
+  MATX_EXIT_HANDLER();
 }
 
 TYPED_TEST(MatMulTestFloatTypes, MediumRectBatched4D)
