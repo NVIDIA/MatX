f1fa93bef5e49280514b0f4d6948215a1666480a
Tyler <tylera@nvidia.com>
2023-02-03 19:00:23 +0000

    fix to support correct specialization for generator printing

--- include/matx/core/tensor_utils.h
+++ include/matx/core/tensor_utils.h
@@ -624,56 +624,6 @@ namespace detail {
 
 static constexpr bool PRINT_ON_DEVICE = false;      ///< Print() uses printf on device
 
-/**
- * @brief Print a tensor's values to stdout
- *
- * This is a wrapper utility function to print the type, size and stride of tensor,
- * see PrintData for details of internal tensor printing options
- *
- * @tparam Args Integral argument types
- * @param op input Operator
- * @param dims Number of values to print for each dimension
- */
-template <typename Op, typename... Args,
-          std::enable_if_t<((std::is_integral_v<Args>)&&...) &&
-                                (Op::Rank() == 0 || sizeof...(Args) > 0),
-                            bool> = true>
-void Print(const Op &op, Args... dims) 
-{
-  MATX_NVTX_START("", matx::MATX_NVTX_LOG_API)
-  
-  // print tensor size info first
-  std::string type = (is_tensor_view_v<Op>) ? "Tensor" : "Operator";
-
-  printf("%s{%s} Rank: %d, Sizes:[", type.c_str(), detail::GetTensorType<typename Op::scalar_type>().c_str(), op.Rank());
-  
-  for (index_t dimIdx = 0; dimIdx < (op.Rank() ); dimIdx++ )
-  {
-    printf("%lld", op.Size(static_cast<int>(dimIdx)) );
-    if( dimIdx < (op.Rank() - 1) )
-      printf(", ");
-  }
-  
-  if constexpr (is_tensor_view_v<Op>) 
-  {
-    printf("], Strides:[");
-    if constexpr (Op::Rank() > 0) 
-    {
-      for (index_t dimIdx = 0; dimIdx < (op.Rank() ); dimIdx++ ) 
-      {
-        printf("%lld", op.Stride(static_cast<int>(dimIdx)) );
-        if( dimIdx < (op.Rank() - 1) )
-        {
-          printf(",");
-        }
-      }   
-    }
-  }
-
-  printf("]\n");
-  PrintData(op, dims...);
-  
-}
 
 /**
  * @brief Print a tensor's values to stdout
@@ -728,6 +678,59 @@ void PrintData(const Op &op, Args... dims) {
   InternalPrint(op, dims...);
 #endif
 }
+
+
+/**
+ * @brief Print a tensor's values to stdout
+ *
+ * This is a wrapper utility function to print the type, size and stride of tensor,
+ * see PrintData for details of internal tensor printing options
+ *
+ * @tparam Args Integral argument types
+ * @param op input Operator
+ * @param dims Number of values to print for each dimension
+ */
+template <typename Op, typename... Args,
+          std::enable_if_t<((std::is_integral_v<Args>)&&...) &&
+                                (Op::Rank() == 0 || sizeof...(Args) > 0),
+                            bool> = true>
+void Print(const Op &op, Args... dims) 
+{
+  MATX_NVTX_START("", matx::MATX_NVTX_LOG_API)
+  
+  // print tensor size info first
+  std::string type = (is_tensor_view_v<Op>) ? "Tensor" : "Operator";
+
+  printf("%s{%s} Rank: %d, Sizes:[", type.c_str(), detail::GetTensorType<typename Op::scalar_type>().c_str(), op.Rank());
+  
+  for (index_t dimIdx = 0; dimIdx < (op.Rank() ); dimIdx++ )
+  {
+    printf("%d", static_cast<int>(op.Size(static_cast<int>(dimIdx))));
+    if( dimIdx < (op.Rank() - 1) )
+      printf(", ");
+  }
+  
+  if constexpr (is_tensor_view_v<Op>) 
+  {
+    printf("], Strides:[");
+    if constexpr (Op::Rank() > 0) 
+    {
+      for (index_t dimIdx = 0; dimIdx < (op.Rank() ); dimIdx++ ) 
+      {
+        printf("%d", static_cast<int>(op.Stride(static_cast<int>(dimIdx))));
+        if( dimIdx < (op.Rank() - 1) )
+        {
+          printf(",");
+        }
+      }   
+    }
+  }
+
+  printf("]\n");
+  PrintData(op, dims...);
+  
+}
+
 
 /**
  * @brief Print a tensor's all values to stdout
