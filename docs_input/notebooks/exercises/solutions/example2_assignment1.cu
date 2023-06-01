b993054f8660432b27ef80c0f0fc8caec9b76cd7
Cliff Burdick <cburdick@nvidia.com>
2023-06-01 12:16:41 -0700

    Adding correct license on files that were wrong

--- docs_input/notebooks/exercises/solutions/example2_assignment1.cu
+++ docs_input/notebooks/exercises/solutions/example2_assignment1.cu
@@ -1,3 +1,35 @@
+////////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (c) 2021, NVIDIA Corporation
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/////////////////////////////////////////////////////////////////////////////////
+
 #include <matx.h>
 
 using namespace matx;
@@ -39,8 +71,8 @@ int main() {
     }
   }
 
-  A.Print();
-  V.Print();
+  print(A);
+  print(V);
   printf("Init verification passed!\n");
 
   /****************************************************************************************************
@@ -63,7 +95,7 @@ int main() {
     }
   }
 
-  A.Print();
+  print(A);
   printf("Sum verification passed!\n");
 
   /****************************************************************************************************
@@ -92,8 +124,8 @@ int main() {
     }
   }
 
-  A.Print();
-  tvs.Print();
+  print(A);
+  print(tvs);
   printf("Clone verification passed!\n");
 
   /****************************************************************************************************
@@ -117,7 +149,7 @@ int main() {
     }
   }
 
-  B.Print();
+  print(B);
   printf("Mixed verification passed!\n");
 
   return 0;
