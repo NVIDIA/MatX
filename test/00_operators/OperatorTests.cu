3657043a4b436b645c7d2550481eafe76d22a039
cliffburdick <cburdick@nvidia.com>
2023-07-05 14:32:13 -0700

    Added upsample and downsample operators

--- test/00_operators/OperatorTests.cu
+++ test/00_operators/OperatorTests.cu
@@ -2984,6 +2984,77 @@ TYPED_TEST(OperatorTestsFloatNonComplexAllExecs, Sphere2Cart)
   MATX_EXIT_HANDLER();
 }
 
+TYPED_TEST(OperatorTestsNumericAllExecs, Upsample)
+{
+  MATX_ENTER_HANDLER();
+  using TestType = std::tuple_element_t<0, TypeParam>;
+  using ExecType = std::tuple_element_t<1, TypeParam>;  
+
+  ExecType exec{}; 
+
+  {
+    // example-begin upsample-test-1
+    // Upsample a signal of length 100 by 5
+    int n = 5;
+
+    auto t1 = make_tensor<TestType>({100});
+    (t1 = static_cast<TestType>(1)).run(exec);
+    auto us_op = upsample(t1, 0, n);
+    // example-end upsample-test-1
+
+    ASSERT_TRUE(us_op.Size(0) == (t1.Size(0) - 1) * n + 1);
+    for (index_t i = 0; i < us_op.Size(0); i++) {
+      if ((i % n) == 0) {
+        MatXUtils::MatXTypeCompare(us_op(i), t1(i / n));
+      }
+      else {
+        MatXUtils::MatXTypeCompare(us_op(i), static_cast<TestType>(0));
+      }
+    }
+  }
+
+  MATX_EXIT_HANDLER();
+}
+
+TYPED_TEST(OperatorTestsNumericAllExecs, Downsample)
+{
+  MATX_ENTER_HANDLER();
+  using TestType = std::tuple_element_t<0, TypeParam>;
+  using ExecType = std::tuple_element_t<1, TypeParam>;  
+
+  ExecType exec{}; 
+
+  {
+    // example-begin downsample-test-1
+    int n = 5;
+
+    auto t1 = make_tensor<TestType>({100});
+    (t1 = static_cast<TestType>(1)).run(exec);
+    auto ds_op = downsample(t1, 0, n);
+    // example-end downsample-test-1
+
+    ASSERT_TRUE(ds_op.Size(0) == t1.Size(0) / n);
+    for (index_t i = 0; i < ds_op.Size(0); i++) {
+      MatXUtils::MatXTypeCompare(ds_op(i), t1(i * n));
+    }
+  }
+
+  {
+    int n = 3;
+
+    auto t1 = make_tensor<TestType>({100});
+    (t1 = static_cast<TestType>(1)).run(exec);
+    auto ds_op = downsample(t1, 0, n);
+
+    ASSERT_TRUE(ds_op.Size(0) == t1.Size(0) / n + 1);
+    for (index_t i = 0; i < ds_op.Size(0); i++) {
+      MatXUtils::MatXTypeCompare(ds_op(i), t1(i * n));
+    }
+  }  
+
+  MATX_EXIT_HANDLER();
+}
+
 TYPED_TEST(OperatorTestsNumericAllExecs, ShiftOp)
 {
   MATX_ENTER_HANDLER();
