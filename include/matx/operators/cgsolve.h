0310f7c7a1ec733138281939807cd1ec429df398
cliffburdick <cburdick@nvidia.com>
2023-07-19 14:26:49 -0700

    cgsolve

---
+++ include/matx/operators/cgsolve.h
@@ -0,0 +1,135 @@
+////////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// COpBright (c) 2021, NVIDIA Corporation
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above cOpBright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above cOpBright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the cOpBright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COpBRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COpBRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+
+#include "matx/core/type_utils.h"
+#include "matx/operators/base_operator.h"
+#include "matx/transforms/cgsolve.h"
+
+namespace matx
+{
+  namespace detail {
+    template <typename OpA, typename OpB>
+    class CGSolveOp : public BaseOp<CGSolveOp<OpA, OpB>>
+    {
+      private:
+        OpA a_;
+        OpB b_;
+        double tol_;
+        int max_iters_;
+        std::array<index_t, 2> out_dims_;
+        matx::tensor_t<typename OpA::scalar_type, 2> tmp_out_;
+
+      public:
+        using matxop = bool;
+        using scalar_type = typename OpA::scalar_type;
+        using matx_transform_op = bool;
+        using matmul_xform_op = bool;
+
+        __MATX_INLINE__ std::string str() const { 
+          return "cgsolve(" + get_type_str(a_) + "," + get_type_str(b_)  + ")";
+        }
+
+        __MATX_INLINE__ CGSolveOp(const OpA &A, const OpB &B, double tol, int max_iters) : 
+              a_(A), b_(B), tol_(tol), max_iters_(max_iters) {
+          
+          for (int r = 0; r < Rank(); r++) {
+            out_dims_[r] = b_.Size(r);
+          }
+        }
+
+        template <typename... Is>
+        __MATX_INLINE__ __MATX_DEVICE__ __MATX_HOST__ auto operator()(Is... indices) const
+        {
+          return tmp_out_(indices...);
+        }
+
+        static __MATX_INLINE__ constexpr __MATX_HOST__ __MATX_DEVICE__ int32_t Rank()
+        {
+          return remove_cvref_t<OpB>::Rank();
+        }
+        constexpr __MATX_INLINE__ __MATX_HOST__ __MATX_DEVICE__ index_t Size(int dim) const
+        {
+          return out_dims_[dim];
+        }
+
+        template <typename Out, typename Executor>
+        void Exec(Out &&out, Executor &&ex) {
+          static_assert(is_device_executor_v<Executor>, "cgsolve() only supports the CUDA executor currently");
+          cgsolve_impl(std::get<0>(out), a_, b_, tol_, max_iters_, ex.getStream());
+        }
+
+        template <typename ShapeType, typename Executor>
+        __MATX_INLINE__ void PreRun([[maybe_unused]] ShapeType &&shape, Executor &&ex) noexcept
+        {
+          if constexpr (is_matx_op<OpA>()) {
+            a_.PreRun(std::forward<ShapeType>(shape), std::forward<Executor>(ex));
+          }     
+
+          if constexpr (is_matx_op<OpB>()) {
+            b_.PreRun(std::forward<ShapeType>(shape), std::forward<Executor>(ex));
+          }          
+
+          if constexpr (is_device_executor_v<Executor>) {
+            make_tensor(tmp_out_, out_dims_, MATX_ASYNC_DEVICE_MEMORY, ex.getStream());
+          }
+
+          Exec(std::make_tuple(tmp_out_), std::forward<Executor>(ex));
+        }
+    };
+  }
+
+
+  /**
+   * Performs a complex gradient solve on a square matrix.  
+   *
+   * @param A
+   *   Tensor A
+   * @param B
+   *   Tensor B 
+   * @param tol
+   *   tolerance to solve to  
+   * @param max_iters
+   *   max iterations for solve
+   *
+   */
+  template <typename AType, typename BType>
+    __MATX_INLINE__ auto cgsolve(AType A, BType B, double tol=1e-6, int max_iters=4)
+  {
+    MATX_NVTX_START("", matx::MATX_NVTX_LOG_API)
+    
+    return detail::CGSolveOp(A, B, tol, max_iters);
+  }
+
+}
