871b14b900a1a81a9bf1146d5676df4b777f978f
Thomas Benson <tbenson@nvidia.com>
2023-09-27 18:25:42 -0700

    Add PreRun methods for slice/fftshift operators
    Add PreRun methods for slice and fftshift operators.
    This allows the operators to be composed with transforms
    (e.g. fftshift1D(fft(x))).

--- test/00_operators/OperatorTests.cu
+++ test/00_operators/OperatorTests.cu
@@ -57,6 +57,9 @@ template <typename TensorType>
 class OperatorTestsFloatNonComplex : public ::testing::Test {
 };
 template <typename TensorType>
+class OperatorTestsFloatNonHalf : public ::testing::Test {
+};
+template <typename TensorType>
 class OperatorTestsFloatNonComplexNonHalf : public ::testing::Test {
 };
 template <typename TensorType>
@@ -121,6 +124,7 @@ TYPED_TEST_SUITE(OperatorTestsNumeric, MatXNumericTypes);
 TYPED_TEST_SUITE(OperatorTestsIntegral, MatXAllIntegralTypes);
 TYPED_TEST_SUITE(OperatorTestsNumericNonComplex,
                  MatXNumericNonComplexTypes);
+TYPED_TEST_SUITE(OperatorTestsFloatNonHalf, MatXFloatNonHalfTypes);
 TYPED_TEST_SUITE(OperatorTestsFloatNonComplex, MatXFloatNonComplexTypes);
 TYPED_TEST_SUITE(OperatorTestsFloatNonComplexNonHalf,
                  MatXFloatNonComplexNonHalfTypes);
@@ -786,6 +790,20 @@ TYPED_TEST(OperatorTestsNumericAllExecs, SliceOp)
       }
     }
   }
+
+  // Test SliceOp applied to a transform, using transpose() as an example transform
+  auto t2trans = make_tensor<TestType>({3, 2});
+  (t2trans = slice(transpose(t2), {2, 1}, {5, 3})).run(exec);
+  cudaStreamSynchronize(0);
+
+  ASSERT_EQ(t2trans.Size(0), 3);
+  ASSERT_EQ(t2trans.Size(1), 2);
+  for (index_t i = 0; i < t2trans.Size(0); i++) {
+    for (index_t j = 0; j < t2trans.Size(1); j++) {
+      ASSERT_EQ(t2trans(i, j), t2(j + 1, i + 2));
+    }
+  }
+
   MATX_EXIT_HANDLER();
 }
 
@@ -3213,6 +3231,101 @@ TYPED_TEST(OperatorTestsNumericAllExecs, Downsample)
       MatXUtils::MatXTypeCompare(ds_op(i), t1(i * n));
     }
   }  
+
+  MATX_EXIT_HANDLER();
+}
+
+TYPED_TEST(OperatorTestsFloatNonHalf, FftShiftWithTransform)
+{
+  MATX_ENTER_HANDLER();
+  using TestType = TypeParam;
+  using inner_type = typename inner_op_type_t<TestType>::type;
+  using complex_type = detail::complex_from_scalar_t<typename inner_op_type_t<TestType>::type>;
+
+  const inner_type thresh = static_cast<inner_type>(1.0e-6);
+
+  // Verify that fftshift1D/ifftshift1D work with nested transforms.
+  // These tests are limited to complex-to-complex transforms where we have matched
+  // dimensions and types for the inputs/outputs. Adding tests that include real-to-complex
+  // or complex-to-real fft compositions is TBD.
+  if constexpr (is_complex_v<TestType>)
+  {
+    const int N1 = 3;
+    const int N2 = 4;
+
+    auto t3 = make_tensor<complex_type>({N1});
+    auto t4 = make_tensor<complex_type>({N2});
+    auto T3 = make_tensor<complex_type>({N1});
+    auto T4 = make_tensor<complex_type>({N2});
+
+    const std::array<complex_type, N1> t3_vals = {{ { 1.0, 0.0 }, { 2.0, 0.0 }, { 3.0, 0.0 } }};
+    const std::array<complex_type, N2> t4_vals = {{ { 1.0, 0.0 }, { 2.0, 0.0 }, { 3.0, 0.0 }, { 4.0, 0.0 } }};
+
+    for (int i = 0; i < N1; i++) { t3(i) = t3_vals[i]; };
+    for (int i = 0; i < N2; i++) { t4(i) = t4_vals[i]; };
+
+    cudaStreamSynchronize(0);
+
+    (T3 = fftshift1D(fft(t3))).run();
+    (T4 = fftshift1D(fft(t4))).run();
+
+    cudaStreamSynchronize(0);
+
+    const std::array<complex_type, N1> T3_expected = {{
+      { -1.5, static_cast<inner_type>(-0.8660254) }, { 6.0, 0.0 }, { -1.5, static_cast<inner_type>(0.8660254) }
+    }};
+    const std::array<complex_type, N2> T4_expected = {{
+      { -2.0, 0.0 }, { -2.0, -2.0 }, { 10.0, 0.0 }, { -2.0, 2.0 }
+    }};
+
+    for (int i = 0; i < N1; i++) {
+      ASSERT_NEAR(T3(i).real(), T3_expected[i].real(), thresh);
+      ASSERT_NEAR(T3(i).imag(), T3_expected[i].imag(), thresh);
+    }
+
+    for (int i = 0; i < N2; i++) {
+      ASSERT_NEAR(T4(i).real(), T4_expected[i].real(), thresh);
+      ASSERT_NEAR(T4(i).imag(), T4_expected[i].imag(), thresh);
+    }
+
+    (T3 = ifftshift1D(fft(t3))).run();
+    (T4 = ifftshift1D(fft(t4))).run();
+
+    cudaStreamSynchronize(0);
+
+    const std::array<complex_type, N1> T3_ifftshift_expected = {{
+      { -1.5, static_cast<inner_type>(0.8660254) }, { -1.5, static_cast<inner_type>(-0.8660254) }, { 6.0, 0.0 }
+    }};
+
+    for (int i = 0; i < N1; i++) {
+      ASSERT_NEAR(T3(i).real(), T3_ifftshift_expected[i].real(), thresh);
+      ASSERT_NEAR(T3(i).imag(), T3_ifftshift_expected[i].imag(), thresh);
+    }
+
+    // For even length vectors, fftshift() and ifftshift() are identical
+    for (int i = 0; i < N2; i++) {
+      ASSERT_NEAR(T4(i).real(), T4_expected[i].real(), thresh);
+      ASSERT_NEAR(T4(i).imag(), T4_expected[i].imag(), thresh);
+    }
+  }
+
+  // Verify that fftshift2D/ifftshift2D work with nested transforms. We do not
+  // check correctness here, but there are fftshift2D correctness tests elsewhere.
+  if constexpr (is_complex_v<TestType>) {
+    [[maybe_unused]] const int N = 4;
+
+    auto x = make_tensor<complex_type>({N,N});
+    auto X = make_tensor<complex_type>({N,N});
+
+    (x = 0).run();
+
+    (X = fftshift2D(fft2(x))).run();
+    (X = fftshift2D(ifft2(x))).run();
+    (X = ifftshift2D(fft2(x))).run();
+    (X = ifftshift2D(ifft2(x))).run();
+
+    cudaStreamSynchronize(0);
+  }
 
   MATX_EXIT_HANDLER();
 }
