4c9b67a45eec7ce85f7c4d9ab65b28fef33c646f
cliffburdick <cburdick@nvidia.com>
2024-01-07 17:47:28 -0800

    outer() operator for outer product

--- include/matx/operators/operators.h
+++ include/matx/operators/operators.h
@@ -75,6 +75,7 @@
 #include "matx/operators/lu.h"
 #include "matx/operators/matmul.h"
 #include "matx/operators/matvec.h"
+#include "matx/operators/outer.h"
 #include "matx/operators/overlap.h"
 #include "matx/operators/percentile.h"
 #include "matx/operators/permute.h"
