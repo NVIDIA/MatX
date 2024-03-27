460cfa49c47799484197b8428f37cab87b245557
Cliff Burdick <30670611+cliffburdick@users.noreply.github.com>
2024-03-26 20:58:04 -0700

    Updated cutensornet to 24.03 and cutensor to 2.0.1 (#600)

--- cmake/FindcuTensorNet.cmake
+++ cmake/FindcuTensorNet.cmake
@@ -81,7 +81,7 @@ endif()
 if(NOT cuTensorNet_FOUND)
   message(STATUS "cuTensorNet not found. Downloading library. By continuing this download you accept to the license terms of cuQuantum SDK")
 
-  set(CUTENSORNET_FILENAME cuquantum-linux-x86_64-${CUTENSORNET_VERSION}-archive)
+  set(CUTENSORNET_FILENAME cuquantum-linux-x86_64-${CUTENSORNET_VERSION}_cuda${CUDAToolkit_VERSION_MAJOR}-archive)
   
   CPMAddPackage(
                NAME cutensornet
@@ -91,7 +91,7 @@ if(NOT cuTensorNet_FOUND)
                DOWNLOAD_ONLY YES 
                )
       
-  set(cuTensorNet_LIBRARY ${cutensornet_SOURCE_DIR}/lib/${CUDAToolkit_VERSION_MAJOR}/libcutensornet.so) 
+  set(cuTensorNet_LIBRARY ${cutensornet_SOURCE_DIR}/lib/libcutensornet.so) 
   set(cuTensorNet_INCLUDE_DIR ${cutensornet_SOURCE_DIR}/include) 
 
   set(cuTensorNet_FOUND TRUE)
--- docs_input/build.rst
+++ docs_input/build.rst
@@ -40,8 +40,8 @@ Optional Third-party Dependencies
 - `GoogleTest <https://github.com/google/googletest>`_ 1.11.0+ (Required to run unit tests)
 - `pybind11 <https://github.com/pybind/pybind11>`_ 2.6.2+ (Required for file I/O and some unit tests)
 - `nvbench <https://github.com/NVIDIA/nvbench>`_ Commit 1a13a2e (Required to run benchmarks)
-- `cutensor <https://developer.nvidia.com/cutensor>`_ 1.7.0.1+ (Required when using `einsum`)
-- `cutensornet <https://docs.nvidia.com/cuda/cuquantum/cutensornet>`_ 23.03.0.20+ (Required when using `einsum`)
+- `cutensor <https://developer.nvidia.com/cutensor>`_ 2.0.1.2+ (Required when using `einsum`)
+- `cutensornet <https://docs.nvidia.com/cuda/cuquantum/cutensornet>`_ 24.03.0.4+ (Required when using `einsum`)
 
 Host (CPU) Support
 ------------------
