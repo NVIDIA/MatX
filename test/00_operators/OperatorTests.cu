f23aff37c923cb5168151dace6d2f5abc4bd4497
Michael Mullen <michael@adialante.com>
2025-01-09 13:59:46 -0600

    change threshold for half types

--- test/00_operators/OperatorTests.cu
+++ test/00_operators/OperatorTests.cu
@@ -2012,13 +2012,18 @@ TYPED_TEST(OperatorTestsFloatAllExecs, Toeplitz)
   MATX_EXIT_HANDLER();
 }
 
-TYPED_TEST(OperatorTestsFloatNonComplexNonHalfAllExecs, Cross)
+TYPED_TEST(OperatorTestsFloatNonComplexAllExecs, Cross)
 {
   MATX_ENTER_HANDLER();
   using TestType = cuda::std::tuple_element_t<0, TypeParam>;
   using ExecType = cuda::std::tuple_element_t<1, TypeParam>;
   ExecType exec{};   
   auto pb = std::make_unique<detail::MatXPybind>();
+  // Half precision needs a bit more tolerance when compared to fp32
+  float thresh = 0.01f;
+  if constexpr (is_matx_half_v<TestType>) {
+    thresh = 0.02f;
+  }
 
   {//batched 4 x 3
     pb->InitAndRunTVGenerator<TestType>("00_operators", "cross_operator", "run", {4, 3});
@@ -2034,7 +2039,7 @@ TYPED_TEST(OperatorTestsFloatNonComplexNonHalfAllExecs, Cross)
     (out = cross(a, b)).run(exec);
     // example-end cross-test-1
     exec.sync();
-    MATX_TEST_ASSERT_COMPARE(pb, out, "out", 0.01);
+    MATX_TEST_ASSERT_COMPARE(pb, out, "out", thresh);
   }
 
   {//non-batched 3
@@ -2048,7 +2053,7 @@ TYPED_TEST(OperatorTestsFloatNonComplexNonHalfAllExecs, Cross)
     
     (out = cross(a, b)).run(exec);
     exec.sync();
-    MATX_TEST_ASSERT_COMPARE(pb, out, "out", 0.01);
+    MATX_TEST_ASSERT_COMPARE(pb, out, "out", thresh);
   }
 
   {//non-batched 2
@@ -2062,7 +2067,7 @@ TYPED_TEST(OperatorTestsFloatNonComplexNonHalfAllExecs, Cross)
     
     (out = cross(a, b)).run(exec);
     exec.sync();
-    MATX_TEST_ASSERT_COMPARE(pb, out, "out", 0.01);
+    MATX_TEST_ASSERT_COMPARE(pb, out, "out", thresh);
   }
 
   MATX_EXIT_HANDLER();
