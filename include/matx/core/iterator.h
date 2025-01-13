1bb2f9c0cf29753b4e4b8be8609d7e034b9369bb
Cliff Burdick <30670611+cliffburdick@users.noreply.github.com>
2025-01-13 12:09:29 -0800

    Fix EndOffset self_type

--- include/matx/core/iterator.h
+++ include/matx/core/iterator.h
@@ -496,7 +496,7 @@ struct BeginOffset {
 
 template <typename OperatorType>
 struct EndOffset {
-  using self_type = BeginOffset<OperatorType>;
+  using self_type = EndOffset<OperatorType>;
   using value_type = index_t;
   // using stride_type = std::conditional_t<is_tensor_view_v<OperatorType>, typename OperatorType::desc_type::stride_type,
   //                         index_t>;
