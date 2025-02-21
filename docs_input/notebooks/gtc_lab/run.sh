34767fbc9d83ee33f089b86eeac87c06f834a2ab
Tyler Allen <tylera@nvidia.com>
2025-02-21 19:51:14 +0000

    cleanup for notebook1

--- docs_input/notebooks/gtc_lab/run.sh
+++ docs_input/notebooks/gtc_lab/run.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
-CMDS="/bin/bash"
-    # -u $USER_ID:$GROU P_ID \
+IMAGE_NAME=gitlab-master.nvidia.com:5005/tylera/playground/gtc-lab:latest
+CMDS="useradd -u $(id -u) -g $(id -g) -m -s /bin/bash tylera && su tylera"
 
 docker run -it --rm \
     -p 8888:8888 \
     --gpus all \
     --ipc=host \
-    -v /home/scratch.tylera_sw/:/scratch \
-    gitlab-master.nvidia.com:5005/devtech-compute/sigx-group/container/cling:latest \
-    /bin/bash
-
-
-
-# docker run -it --rm \
-#     -p 8888:8888 \
-#     --gpus all \
-#     -v /home/scratch.tylera_sw/:/scratch \
-#     -v /home/scratch.tylera_sw/projects/matx_holo_lab_2025:/notebooks \
-#     --env="DISPLAY" \
-#     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-#     --cap-add CAP_SYS_PTRACE \
-#     --ipc=host \
-#     --ulimit memlock=-1 \
-#     --ulimit stack=67108864 \
-#     gitlab-master.nvidia.com:5005/devtech-compute/sigx-group/container/cling:latest \
-#     /bin/bash
+    -v /scratch/tylera/:/scratch/tylera \
+    -w $(pwd) \
+    $IMAGE_NAME 
