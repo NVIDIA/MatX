f27cf1a237127dd0e8630a7b4a63a24e1c846426
Aart J.C. Bik <abik@nvidia.com>
2025-02-24 10:46:56 -0800

    braces

--- include/matx/transforms/convert/sparse2dense_cusparse.h
+++ include/matx/transforms/convert/sparse2dense_cusparse.h
@@ -210,10 +210,11 @@ using sparse2dense_cache_t =
 template <typename Op>
 __MATX_INLINE__ auto getS2DSupportedTensor(const Op &in, cudaStream_t stream) {
   const auto func = [&]() {
-    if constexpr (is_tensor_view_v<Op>)
+    if constexpr (is_tensor_view_v<Op>) {
       return in.Stride(Op::Rank() - 1) == 1;
-    else
+    } else {
       return true;
+    }
   };
   return GetSupportedTensor(in, func, MATX_ASYNC_DEVICE_MEMORY, stream);
 }
