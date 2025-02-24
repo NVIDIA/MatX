5f02c03a9281727bb4dc476877908df7cc58b0f6
Aart J.C. Bik <abik@nvidia.com>
2025-02-24 10:48:41 -0800

    braces

--- include/matx/transforms/solve/solve_cudss.h
+++ include/matx/transforms/solve/solve_cudss.h
@@ -224,10 +224,11 @@ template <typename Op>
 __MATX_INLINE__ auto getSolveSupportedTensor(const Op &in,
                                              cudaStream_t stream) {
   const auto func = [&]() {
-    if constexpr (is_tensor_view_v<Op>)
+    if constexpr (is_tensor_view_v<Op>) {
       return in.Stride(Op::Rank() - 1) == 1;
-    else
+    } else {
       return true;
+    }
   };
   return GetSupportedTensor(in, func, MATX_ASYNC_DEVICE_MEMORY, stream);
 }
