7d0380e6b00cd6f56c676124b0b8adb5196a9fd8
Tyler Allen <tylera@nvidia.com>
2025-02-24 23:16:02 +0000

    updates for limitations section of fusion. mount entire scratch. and custom scalar_ops function

--- docs_input/notebooks/gtc_lab/run.sh
+++ docs_input/notebooks/gtc_lab/run.sh
@@ -3,16 +3,15 @@ USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 IMAGE_NAME=gitlab-master.nvidia.com:5005/tylera/playground/gtc-lab:latest
 
-
 LAB_FOLDER=$(pwd)
 MATX_ROOT_DIR="${LAB_FOLDER%/*/*/*}/"
-echo $MATX_ROOT_DIR
+SCRATCH_DIR="/$(echo "$LAB_FOLDER" | cut -d'/' -f2-3)/"
 
 docker run -it --rm \
     -p 8888:8888 \
     --gpus all \
     --ipc=host \
     -v $MATX_ROOT_DIR/:/opt/xeus/cling/tools/Jupyter/kernel/MatX/ \
-    -v $LAB_FOLDER:$LAB_FOLDER \
+    -v $SCRATCH_DIR:$SCRATCH_DIR \
     -w $LAB_FOLDER \
     $IMAGE_NAME 
