f5a2a7b81ed26f10cfbfdb085c74ffd22455b6a6
Dylan Eustice <deustice@nvidia.com>
2025-02-27 19:09:09 -0500

    Allow run.sh to be run from anywhere

--- docs_input/notebooks/gtc_lab/run.sh
+++ docs_input/notebooks/gtc_lab/run.sh
@@ -3,7 +3,7 @@ USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 IMAGE_NAME=gitlab-master.nvidia.com:5005/tylera/playground/gtc-lab:latest
 
-LAB_FOLDER=$(pwd)
+LAB_FOLDER="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 MATX_ROOT_DIR="${LAB_FOLDER%/*/*/*}/"
 SCRATCH_DIR="/$(echo "$LAB_FOLDER" | cut -d'/' -f2-3)/"
 
@@ -14,4 +14,4 @@ docker run -it --rm \
     -v $MATX_ROOT_DIR/:/opt/xeus/cling/tools/Jupyter/kernel/MatX/ \
     -v $SCRATCH_DIR:$SCRATCH_DIR \
     -w $LAB_FOLDER \
-    $IMAGE_NAME 
+    $IMAGE_NAME
