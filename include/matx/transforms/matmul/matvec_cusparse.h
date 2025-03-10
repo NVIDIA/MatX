01a92dad1d1ecd3f63d3e15af0b6bcbc94c097db
Aart J.C. Bik <abik@nvidia.com>
2025-03-10 11:40:12 -0700

    Add SpMV support for matvec transformation
    with tests and doc

--- include/matx/transforms/matmul/matvec_cusparse.h
+++ include/matx/transforms/matmul/matvec_cusparse.h
@@ -46,22 +46,19 @@ namespace matx {
 namespace detail {
 
 /**
- * Parameters needed to execute a cuSPARSE GEMM.
+ * Parameters needed to execute a cuSPARSE SpMV.
  */
-struct MatMulCUSPARSEParams_t {
+struct MatVecCUSPARSEParams_t {
   MatXDataType_t dtype;
   MatXDataType_t ptype;
   MatXDataType_t ctype;
-  int rank;
   cudaStream_t stream;
   float alpha;
   float beta;
   index_t nse;
   index_t m;
   index_t n;
-  index_t k;
   cusparseOperation_t opA;
-  cusparseOperation_t opB;
   // Matrix handles in cuSPARSE are data specific (unlike e.g. cuBLAS
   // where the same plan can be shared between different data buffers).
   void *ptrA0;
@@ -74,22 +71,20 @@ struct MatMulCUSPARSEParams_t {
 };
 
 template <typename TensorTypeC, typename TensorTypeA, typename TensorTypeB>
-class MatMulCUSPARSEHandle_t {
+class MatVecCUSPARSEHandle_t {
 public:
   using TA = typename TensorTypeA::value_type;
   using TB = typename TensorTypeB::value_type;
   using TC = typename TensorTypeC::value_type;
 
   /**
-   * Construct a sparse GEMM handle
-   *   SpMM        <- for now
-   *   SpGEMM
+   * Construct a SpMV handle
    */
-  MatMulCUSPARSEHandle_t(TensorTypeC &c, const TensorTypeA &a,
+  MatVecCUSPARSEHandle_t(TensorTypeC &c, const TensorTypeA &a,
                          const TensorTypeB &b, cudaStream_t stream, float alpha,
                          float beta) {
     MATX_NVTX_START("", matx::MATX_NVTX_LOG_INTERNAL)
-    params_ = GetGemmParams(c, a, b, stream, alpha, beta);
+    params_ = GetSpMVParams(c, a, b, stream, alpha, beta);
 
     // Properly typed alpha, beta.
     if constexpr (std::is_same_v<TC, cuda::std::complex<float>> ||
@@ -101,7 +96,7 @@ public:
       salpha_ = alpha;
       sbeta_ = beta;
     } else {
-      MATX_THROW(matxNotSupported, "SpMM currently only supports uniform FP");
+      MATX_THROW(matxNotSupported, "SpMV currently only supports uniform FP");
     }
 
     [[maybe_unused]] cusparseStatus_t ret = cusparseCreate(&handle_);
@@ -116,72 +111,66 @@ public:
     cusparseIndexBase_t zb = CUSPARSE_INDEX_BASE_ZERO;
     cudaDataType dta = MatXTypeToCudaType<TA>();
     if constexpr (TensorTypeA::Format::isCOO()) {
-      ret = cusparseCreateCoo(&matA_, params_.m, params_.k, params_.nse,
+      ret = cusparseCreateCoo(&matA_, params_.m, params_.n, params_.nse,
                               params_.ptrA3, params_.ptrA4, params_.ptrA0, ct,
                               zb, dta);
     } else if constexpr (TensorTypeA::Format::isCSR()) {
-      ret = cusparseCreateCsr(&matA_, params_.m, params_.k, params_.nse,
+      ret = cusparseCreateCsr(&matA_, params_.m, params_.n, params_.nse,
                               params_.ptrA2, params_.ptrA4, params_.ptrA0, pt,
                               ct, zb, dta);
     } else if constexpr (TensorTypeA::Format::isCSC()) {
-      ret = cusparseCreateCsc(&matA_, params_.m, params_.k, params_.nse,
+      ret = cusparseCreateCsc(&matA_, params_.m, params_.n, params_.nse,
                               params_.ptrA2, params_.ptrA4, params_.ptrA0, pt,
                               ct, zb, dta);
     } else {
-      MATX_THROW(matxNotSupported, "SpMM currently only supports COO/CSR/CSC");
+      MATX_THROW(matxNotSupported, "SpMV currently only supports COO/CSR/CSC");
     }
     MATX_ASSERT(ret == CUSPARSE_STATUS_SUCCESS, matxMatMulError);
 
-    // Create cuSPARSE handle for dense matrices B and C.
+    // Create cuSPARSE handle for dense vectors B and C.
     static_assert(is_tensor_view_v<TensorTypeB>);
     static_assert(is_tensor_view_v<TensorTypeC>);
     cudaDataType dtb = MatXTypeToCudaType<TB>();
     cudaDataType dtc = MatXTypeToCudaType<TC>();
-    const cusparseOrder_t order = CUSPARSE_ORDER_ROW;
-    ret = cusparseCreateDnMat(&matB_, params_.k, params_.n, /*ld=*/params_.n,
-                              params_.ptrB, dtb, order);
+    ret = cusparseCreateDnVec(&vecB_, params_.n, params_.ptrB, dtb);
     MATX_ASSERT(ret == CUSPARSE_STATUS_SUCCESS, matxMatMulError);
-    ret = cusparseCreateDnMat(&matC_, params_.m, params_.n, /*ld=*/params_.n,
-                              params_.ptrC, dtc, order);
+    ret = cusparseCreateDnVec(&vecC_, params_.m, params_.ptrC, dtc);
     MATX_ASSERT(ret == CUSPARSE_STATUS_SUCCESS, matxMatMulError);
 
-    // Allocate a workspace for SpMM.
-    const cusparseSpMMAlg_t algo = CUSPARSE_SPMM_ALG_DEFAULT;
+    // Allocate a workspace for SpMV.
+    const cusparseSpMVAlg_t algo = CUSPARSE_SPMV_ALG_DEFAULT;
     const cudaDataType comptp = dtc; // TODO: support separate comp type?!
-    ret = cusparseSpMM_bufferSize(handle_, params_.opA, params_.opB, &salpha_,
-                                  matA_, matB_, &sbeta_, matC_, comptp, algo,
-                                  &workspaceSize_);
+    ret =
+        cusparseSpMV_bufferSize(handle_, params_.opA, &salpha_, matA_, vecB_,
+                                &sbeta_, vecC_, comptp, algo, &workspaceSize_);
     MATX_ASSERT(ret == CUSPARSE_STATUS_SUCCESS, matxMatMulError);
     if (workspaceSize_) {
       matxAlloc((void **)&workspace_, workspaceSize_, MATX_DEVICE_MEMORY);
     }
   }
 
-  ~MatMulCUSPARSEHandle_t() {
+  ~MatVecCUSPARSEHandle_t() {
     if (workspaceSize_) {
       matxFree(workspace_);
     }
     cusparseDestroy(handle_);
   }
 
-  static detail::MatMulCUSPARSEParams_t
-  GetGemmParams(TensorTypeC &c, const TensorTypeA &a, const TensorTypeB &b,
+  static detail::MatVecCUSPARSEParams_t
+  GetSpMVParams(TensorTypeC &c, const TensorTypeA &a, const TensorTypeB &b,
                 cudaStream_t stream, float alpha, float beta) {
-    detail::MatMulCUSPARSEParams_t params;
+    detail::MatVecCUSPARSEParams_t params;
     params.dtype = TypeToInt<typename TensorTypeA::val_type>();
     params.ptype = TypeToInt<typename TensorTypeA::pos_type>();
     params.ctype = TypeToInt<typename TensorTypeA::crd_type>();
-    params.rank = c.Rank();
     params.stream = stream;
     params.alpha = alpha;
     params.beta = beta;
     // TODO: simple no-batch, row-wise, no-transpose for now
     params.nse = a.Nse();
     params.m = a.Size(TensorTypeA::Rank() - 2);
-    params.n = b.Size(TensorTypeB::Rank() - 1);
-    params.k = a.Size(TensorTypeA::Rank() - 1);
+    params.n = a.Size(TensorTypeA::Rank() - 1);
     params.opA = CUSPARSE_OPERATION_NON_TRANSPOSE;
-    params.opB = CUSPARSE_OPERATION_NON_TRANSPOSE;
     // Matrix handles in cuSPARSE are data specific. Therefore, the pointers
     // to the underlying buffers are part of the GEMM parameters.
     params.ptrA0 = a.Data();
@@ -198,22 +187,22 @@ public:
                             [[maybe_unused]] const TensorTypeA &a,
                             [[maybe_unused]] const TensorTypeB &b) {
     MATX_NVTX_START("", matx::MATX_NVTX_LOG_INTERNAL);
-    const cusparseSpMMAlg_t algo = CUSPARSE_SPMM_ALG_DEFAULT;
+    const cusparseSpMVAlg_t algo = CUSPARSE_SPMV_ALG_DEFAULT;
     const cudaDataType comptp = MatXTypeToCudaType<TC>(); // TODO: see above
     [[maybe_unused]] cusparseStatus_t ret =
-        cusparseSpMM(handle_, params_.opA, params_.opB, &salpha_, matA_, matB_,
-                     &sbeta_, matC_, comptp, algo, workspace_);
+        cusparseSpMV(handle_, params_.opA, &salpha_, matA_, vecB_, &sbeta_,
+                     vecC_, comptp, algo, workspace_);
     MATX_ASSERT(ret == CUSPARSE_STATUS_SUCCESS, matxMatMulError);
   }
 
 private:
   cusparseHandle_t handle_ = nullptr; // TODO: share handle globally?
   cusparseSpMatDescr_t matA_ = nullptr;
-  cusparseDnMatDescr_t matB_ = nullptr;
-  cusparseDnMatDescr_t matC_ = nullptr;
+  cusparseDnVecDescr_t vecB_ = nullptr;
+  cusparseDnVecDescr_t vecC_ = nullptr;
   size_t workspaceSize_ = 0;
   void *workspace_ = nullptr;
-  detail::MatMulCUSPARSEParams_t params_;
+  detail::MatVecCUSPARSEParams_t params_;
   TC salpha_;
   TC sbeta_;
 };
@@ -223,8 +212,8 @@ private:
  * doesn't need to be perfect, but fast enough to not slow down lookups, and
  * different enough so the common GEMM parameters change.
  */
-struct MatMulCUSPARSEParamsKeyHash {
-  std::size_t operator()(const MatMulCUSPARSEParams_t &k) const noexcept {
+struct MatVecCUSPARSEParamsKeyHash {
+  std::size_t operator()(const MatVecCUSPARSEParams_t &k) const noexcept {
     return std::hash<uint64_t>()(reinterpret_cast<uint64_t>(k.ptrA0)) +
            std::hash<uint64_t>()(reinterpret_cast<uint64_t>(k.ptrB)) +
            std::hash<uint64_t>()(reinterpret_cast<uint64_t>(k.ptrC)) +
@@ -236,25 +225,24 @@ struct MatMulCUSPARSEParamsKeyHash {
  * Test GEMM parameters for equality. Unlike the hash, all parameters must
  * match exactly to ensure the hashed kernel can be reused for the computation.
  */
-struct MatMulCUSPARSEParamsKeyEq {
-  bool operator()(const MatMulCUSPARSEParams_t &l,
-                  const MatMulCUSPARSEParams_t &t) const noexcept {
+struct MatVecCUSPARSEParamsKeyEq {
+  bool operator()(const MatVecCUSPARSEParams_t &l,
+                  const MatVecCUSPARSEParams_t &t) const noexcept {
     return l.dtype == t.dtype && l.ptype == t.ptype && l.ctype == t.ctype &&
-           l.rank == t.rank && l.stream == t.stream && l.alpha == t.alpha &&
-           l.beta == t.beta && l.nse == t.nse && l.m == t.m && l.n == t.n &&
-           l.k == t.k && l.opA == t.opA && l.opB == t.opB &&
+           l.stream == t.stream && l.alpha == t.alpha && l.beta == t.beta &&
+           l.nse == t.nse && l.m == t.m && l.n == t.n && l.opA == t.opA &&
            l.ptrA0 == t.ptrA0 && l.ptrA1 == t.ptrA1 && l.ptrA2 == t.ptrA2 &&
            l.ptrA3 == t.ptrA3 && l.ptrA4 == t.ptrA4 && l.ptrB == t.ptrB &&
            l.ptrC == t.ptrC;
   }
 };
 
-using gemm_cusparse_cache_t =
-    std::unordered_map<MatMulCUSPARSEParams_t, std::any,
-                       MatMulCUSPARSEParamsKeyHash, MatMulCUSPARSEParamsKeyEq>;
+using spmv_cusparse_cache_t =
+    std::unordered_map<MatVecCUSPARSEParams_t, std::any,
+                       MatVecCUSPARSEParamsKeyHash, MatVecCUSPARSEParamsKeyEq>;
 
 template <typename Op>
-__MATX_INLINE__ auto getCuSparseGemmSupportedTensor(const Op &in,
+__MATX_INLINE__ auto getCuSparseSpMVSupportedTensor(const Op &in,
                                                     cudaStream_t stream) {
   const auto func = [&]() {
     if constexpr (is_tensor_view_v<Op>) {
@@ -269,15 +257,15 @@ __MATX_INLINE__ auto getCuSparseGemmSupportedTensor(const Op &in,
 } // end namespace detail
 
 template <typename TensorTypeC, typename TensorTypeA, typename TensorTypeB>
-void sparse_matmul_impl(TensorTypeC &C, const TensorTypeA &a,
+void sparse_matvec_impl(TensorTypeC &C, const TensorTypeA &a,
                         const TensorTypeB &B, const cudaExecutor &exec,
                         float alpha = 1.0, float beta = 0.0) {
   MATX_NVTX_START("", matx::MATX_NVTX_LOG_API)
   const auto stream = exec.getStream();
 
   // Transform into supported form.
-  auto b = getCuSparseGemmSupportedTensor(B, stream);
-  auto c = getCuSparseGemmSupportedTensor(C, stream);
+  auto b = getCuSparseSpMVSupportedTensor(B, stream);
+  auto c = getCuSparseSpMVSupportedTensor(C, stream);
   if (!is_matx_transform_op<TensorTypeB>() && !b.isSameView(B)) {
     (b = B).run(stream);
   }
@@ -295,8 +283,8 @@ void sparse_matmul_impl(TensorTypeC &C, const TensorTypeA &a,
   static constexpr int RANKC = ctype::Rank();
 
   // Restrictions.
-  static_assert(RANKA == 2 && RANKB == 2 && RANKC == 2,
-                "tensors must have rank-2");
+  static_assert(RANKA == 2 && RANKB == 1 && RANKC == 1,
+                "tensors must have SpMV rank");
   static_assert(std::is_same_v<TC, TA> && std::is_same_v<TC, TB>,
                 "tensors must have the same data type");
   // TODO: allow MIXED-PRECISION computation!
@@ -304,22 +292,21 @@ void sparse_matmul_impl(TensorTypeC &C, const TensorTypeA &a,
                     std::is_same_v<TC, cuda::std::complex<float>> ||
                     std::is_same_v<TC, cuda::std::complex<double>>,
                 "unsupported data type");
-  MATX_ASSERT(a.Size(RANKA - 1) == b.Size(RANKB - 2) &&
-                  c.Size(RANKC - 1) == b.Size(RANKB - 1) &&
-                  c.Size(RANKC - 2) == a.Size(RANKA - 2),
+  MATX_ASSERT(a.Size(RANKA - 1) == b.Size(RANKB - 1) &&
+                  a.Size(RANKA - 2) == c.Size(RANKC - 1),
               matxInvalidSize);
   MATX_ASSERT(b.Stride(RANKB - 1) == 1 && c.Stride(RANKC - 1) == 1,
               matxInvalidParameter);
 
   // Get parameters required by these tensors (for caching).
   auto params =
-      detail::MatMulCUSPARSEHandle_t<ctype, atype, btype>::GetGemmParams(
+      detail::MatVecCUSPARSEHandle_t<ctype, atype, btype>::GetSpMVParams(
           c, a, b, stream, alpha, beta);
 
   // Lookup and cache.
-  using cache_val_type = detail::MatMulCUSPARSEHandle_t<ctype, atype, btype>;
-  detail::GetCache().LookupAndExec<detail::gemm_cusparse_cache_t>(
-      detail::GetCacheIdFromType<detail::gemm_cusparse_cache_t>(), params,
+  using cache_val_type = detail::MatVecCUSPARSEHandle_t<ctype, atype, btype>;
+  detail::GetCache().LookupAndExec<detail::spmv_cusparse_cache_t>(
+      detail::GetCacheIdFromType<detail::spmv_cusparse_cache_t>(), params,
       [&]() {
         return std::make_shared<cache_val_type>(c, a, b, stream, alpha, beta);
       },
