9510984b739ef994140c1639b2120d0663853be2
Cliff Burdick <30670611+cliffburdick@users.noreply.github.com>
2025-04-20 08:00:41 -0700

    Fixing lvalue error on newer compilers

--- include/matx/operators/set.h
+++ include/matx/operators/set.h
@@ -110,10 +110,12 @@ public:
   {
     if constexpr (is_matx_half_v<T> &&
                   std::is_integral_v<decltype(detail::get_value(op_, indices...))>) {
-      out_(indices...) = static_cast<float>(detail::get_value(op_, indices...));
+      auto &&out = static_cast<float>(detail::get_value(op_, indices...));
+      out_(indices...) = out;
     }
     else {
-      out_(indices...) = detail::get_value(op_, indices...);
+      auto &&out = detail::get_value(op_, indices...);
+      out = out_(indices...);
     }
 
     return out_(indices...);
