973886b3747f0ece33151a1d195716387ef0335f
Albert Sidelnik <asidelnik@nvidia.com>
2025-04-21 15:23:03 -0700

    update test script for radar code

--- examples/simple_radar_pipeline_test.sh
+++ examples/simple_radar_pipeline_test.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 COMMAND_PARAMETERS=("../build/examples/simple_radar_pipeline" "../build/examples/simple_radar_pipeline_stf")
-STREAM_PARAMETERS=("--numStreams 1" "--numStreams 2" "--numStreams 4" "--numStreams 8")
+STREAM_PARAMETERS=("1" "2" "4" "5" "6")
 #SAMPLE_PARAMETERS=("--numSamples 1000" "--numSamples 4500" "--numSamples 9000")
-SAMPLE_PARAMETERS=("--numSamples 1000" "--numSamples 2000" "--numSamples 4500" "--numSamples 6000")
+SAMPLE_PARAMETERS=("1000" "2000" "4500" "6000" "10000")
 
 NUM_RUNS=5
 
 OUTPUT_FILE="radar_heatmap_data.csv"
 # Initialize the CSV file
-echo "Command,NumStreams,NumSamples,AverageGbps" > $OUTPUT_FILE
 
 # Loop through the parameters
-for command_param in "${COMMAND_PARAMETERS[@]}"; do
-  for str_param in "${STREAM_PARAMETERS[@]}"; do
+for str_param in "${STREAM_PARAMETERS[@]}"; do
+  OUTPUT_FILE="radar_heatmap_data_${sam_param}.csv"
+  echo "Command,NumStreams,NumSamples,AverageGbps" > $OUTPUT_FILE
+  for command_param in "${COMMAND_PARAMETERS[@]}"; do
     for sam_param in "${SAMPLE_PARAMETERS[@]}"; do
       TOTAL_GBPS=0
       for i in $(seq 1 $NUM_RUNS); do
         #echo "Iteration $i with parameter $command_param $str_param $sam_param"
-        OUTPUT=$($command_param $str_param $sam_param)
+        OUTPUT=$($command_param --numStreams $str_param --numSamples $sam_param)
         GBPS=$(echo "$OUTPUT" | grep -oP '(?<=\().*? Gbps' | awk '{print $1}')
 
         # Add the extracted value to the total
@@ -34,7 +35,8 @@ for command_param in "${COMMAND_PARAMETERS[@]}"; do
         AVERAGE_GBPS=$(echo "$TOTAL_GBPS / $NUM_RUNS" | bc -l)
         #echo "$command_param $str_param $sam_param verage Gbps over $NUM_RUNS runs: $AVERAGE_GBPS"
         # Append the results to the CSV file
-        echo "$command_param,$(echo $str_param | awk '{print $2}'),$(echo $sam_param | awk '{print $2}'),$AVERAGE_GBPS" >> $OUTPUT_FILE
+        echo "$command_param,$(echo $str_param | awk '{print $0}'),$(echo $sam_param | awk '{print $0}'),$AVERAGE_GBPS"
+        echo "$command_param,$(echo $str_param | awk '{print $0}'),$(echo $sam_param | awk '{print $0}'),$AVERAGE_GBPS" >> $OUTPUT_FILE
       else
         echo "No runs were performed."
       fi
