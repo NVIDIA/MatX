af1c63b774c53b7cbe3aae06a008ed4ca2f73919
Cliff Burdick <30670611+cliffburdick@users.noreply.github.com>
2026-01-29 08:51:34 -0800

    Fix static_assert message for qr_solver usage

--- include/matx/operators/qr.h
+++ include/matx/operators/qr.h
@@ -149,7 +149,7 @@ namespace detail {
 
       template <typename Out, typename Executor>
       void Exec(Out &&out, Executor &&ex) {
-        static_assert(cuda::std::tuple_size_v<remove_cvref_t<Out>> == 3, "Must use mtie with 2 outputs on qr_solver(). ie: (mtie(A, tau) = eig(A))");     
+        static_assert(cuda::std::tuple_size_v<remove_cvref_t<Out>> == 3, "Must use mtie with 2 outputs on qr_solver(). ie: (mtie(A, tau) = qr_solver(A))");     
 
         qr_solver_impl(cuda::std::get<0>(out), cuda::std::get<1>(out), a_, ex);
       }
