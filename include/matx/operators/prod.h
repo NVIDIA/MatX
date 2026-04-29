38aeb2963ca2c0318b562a5d84a19c1bc6dcaba8
Thomas Benson <tbenson@nvidia.com>
2026-04-29 12:39:35 -0700

    Correct typos in license headers
    We have accumulated several typos in copyright headers, likely due to find-replace
    or auto-completion. Fix these issues across the repo.
    
    Signed-off-by: Thomas Benson <tbenson@nvidia.com>

--- include/matx/operators/prod.h
+++ include/matx/operators/prod.h
@@ -2,7 +2,7 @@
 // BSD 3-Clause License
 //
 // Copyright (c) 2021, NVIDIA Corporation
-// prod rights reserved.
+// All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -18,10 +18,10 @@
 //    contributors may be used to endorse or promote products derived from
 //    this software without specific prior written permission.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COpBRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHprod THE COpBRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
 // FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
 // DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
 // SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
